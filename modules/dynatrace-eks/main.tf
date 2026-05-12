af954b89d8509790eceedec7f9cafbce958b9dfe
Andrew Ramires May <andrewc3po@gmail.com>
2026-05-12 10:26:59 -0400

    git push

--- modules/dynatrace-eks/main.tf
+++ modules/dynatrace-eks/main.tf
@@ -26,17 +26,8 @@ provider "helm" {
 provider "kubectl" {
   host                   = data.aws_eks_cluster.this.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.this.token
   load_config_file       = false
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args = [
-      "eks", "get-token",
-      "--cluster-name", var.eks_cluster_name,
-      "--region", coalesce(var.aws_region, data.aws_region.current.id),
-    ]
-  }
 }
 
 resource "kubernetes_namespace_v1" "dynatrace" {
