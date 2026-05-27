6c827e5ea022fa425ff8c712018cab44f7f2f995
Andrew Ramires May <andrewc3po@gmail.com>
2026-05-27 11:06:31 -0400

    Melhoria no padrão do projeto

--- modules/dynatrace-eks/main.tf
+++ modules/dynatrace-eks/main.tf
@@ -1,33 +1,7 @@
-
 data "aws_region" "current" {}
 
 data "aws_eks_cluster" "this" {
   name = var.eks_cluster_name
-}
-
-data "aws_eks_cluster_auth" "this" {
-  name = var.eks_cluster_name
-}
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-}
-
-provider "helm" {
-  kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
-  }
-}
-
-provider "kubectl" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
-  load_config_file       = false
 }
 
 resource "kubernetes_namespace_v1" "dynatrace" {
